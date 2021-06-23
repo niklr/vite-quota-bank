c4e69a87f0751fc1c682068f0772e87f72651c47
niklr <r.niklaus@gmx.ch>
2021-06-23 08:48:28 +0200

    Add styled-components dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
+const Container = styled.div`
+  text-align: center;
+`
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Button>Press me?</Button>
+    </Container>
   );
 }
 
