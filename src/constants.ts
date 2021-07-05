a6ed361d05942da8812468e781f26610ebac50c8
niklr <r.niklaus@gmx.ch>
2021-07-05 22:44:01 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,7 @@ export const AppConstants = {
   InitialNetworkBlockHeight: "0",
   ViteTokenId: "tti_5649544520544f4b454e6e40",
   QuotaContractAddress: "vite_0000000000000000000000000000000000000003f6af7459b9",
+  BankContractAddress: "vite_de2f8c486c0182eaefd83678fc088fa2db5da7d33a4e7cd825",
   NetworkStorageSpace: 'VQB_NETWORK',
   WebWalletStorageSpace: 'VQB_WEB_WALLET'
 }
