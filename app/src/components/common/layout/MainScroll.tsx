6c5771f4b24cfbb01b34fcd64921f988b1c09d62
niklr <r.niklaus@gmx.ch>
2021-07-07 09:37:38 +0200

    Fix MainScroll

--- app/src/components/common/layout/MainScroll.tsx
+++ app/src/components/common/layout/MainScroll.tsx
@@ -1,46 +1,32 @@
 import { Container } from '@material-ui/core'
 import * as React from 'react'
 import styled from 'styled-components'
-import { theme } from '../../../common/theme'
 
 const MainScrollStyled = styled.div`
-  align-items: center;
   display: flex;
   flex-direction: column;
   flex-grow: 1;
   align-items: center;
   padding-bottom: 15px;
-  padding-top: 64px;
-  position: relative;
-  z-index: 2;
-
-  @media (min-width: ${theme.breakpoints.values.md}) {
-    overflow: auto;
-    overflow-x: hidden;
-  }
+  padding-top: 30px;
+  overflow: auto;
+  overflow-x: hidden;
 `
 
 const MainScrollInner = styled.div`
   display: flex;
   flex-direction: column;
-  flex-grow: 1;
-  flex-shrink: 0;
-  margin: 0 auto;
-  max-width: 100%;
   align-items: center;
-  padding-left: 10px;
-  padding-right: 10px;
-  width: ${theme.breakpoints.values.xl};
 `
 
 export const MainScroll: React.FC = props => {
   const { children, ...restProps } = props
 
   return (
-    <Container maxWidth="lg">
-      <MainScrollStyled {...restProps}>
+    <MainScrollStyled {...restProps}>
+      <Container maxWidth="lg">
         <MainScrollInner>{children}</MainScrollInner>
-      </MainScrollStyled>
-    </Container>
+      </Container>
+    </MainScrollStyled>
   )
 }
